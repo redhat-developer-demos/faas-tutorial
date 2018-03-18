d999d916b95f91091c73dfafb419e2e714602bd8
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-03-18 21:54:38 +0530

    Fixing NPE in test

--- hello-web/src/test/java/com/example/FunctionAppTest.java
+++ hello-web/src/test/java/com/example/FunctionAppTest.java
@@ -3,8 +3,6 @@ package com.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.util.Base64;
-
 import com.google.gson.JsonObject;
 
 import org.junit.Test;
@@ -19,10 +17,7 @@ public class FunctionAppTest {
     args.addProperty("name", "test");
     JsonObject response = FunctionApp.main(args);
     assertNotNull(response);
-    //the request data "{"name": "test"}" will be returned with Bas64 encoded string
-    //within JSON attribute __ow_body
-    String base64ReqBody = response.getAsJsonPrimitive("__ow_body").getAsString();
-    String actual = new String(Base64.getDecoder().decode(base64ReqBody.getBytes()));
-    assertEquals("{\"name\": \"test\"}", actual);
+    String actual = response.get("response").getAsJsonObject().get("name").getAsString();
+    assertEquals("test", actual);
   }
 }
